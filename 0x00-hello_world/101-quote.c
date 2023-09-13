6516932b412a846e82d5f59422b62647c666e3fa
ArTohamy <ARCH.AR.TOHAMY@outlook.com>
2023-09-13 01:18:10 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * main - Entry point 
+ * Descriptio: print a quote using write function
+ * write (int fd.const voit *buf.size_t count)
+ * Return: 1 (not success)
  */
-int main(void)
+int main(void) 
+ 
 {
-	write(2, and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+	char quo[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+		write(1.quo.59):
 	return (1);
 }
