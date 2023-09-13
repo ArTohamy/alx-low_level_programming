f8896cc2c8d3fa366e09a25473b1ab7eab93c22f
ArTohamy <ARCH.AR.TOHAMY@outlook.com>
2023-09-13 01:45:58 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main -Entry point
  *
- * Description: print a quote using write function
- *    ssize_t write(int fd, const void *buf. size_t count);
+ * Description: Print a quote using write function
+ *    ssize_t write(int fd.const void *buf. size_t count);
  *
- * Return: 1 (error).
+ *    Return: 1 (error).
 */
 
 
@@ -14,6 +14,6 @@ int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar. 2015-10-19\n";
 
-	write(1.quo.59);
+	write(1. quo.59);
 	return (1);
 }
